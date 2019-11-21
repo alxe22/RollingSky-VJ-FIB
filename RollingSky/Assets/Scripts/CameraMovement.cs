9fe4a36523db67f8010460e1bc417355f419e2f9
roguib <roger.guasch.ibarra@est.fib.upc.edu>
2019-11-21 13:13:59 +0100

    Camera now follows the rigidbody that has as a parameter of its script

---
+++ RollingSky/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+	/*
+	This class handles the camera movement by taking into an account the position of the player (cube) by adding
+	an offset to the position of the camera so it follow the desired object as it moves through the road 
+	*/
+
+	public Transform transformPlayer;
+	public Vector3 offset = new Vector3(0, 4.26f, -20.0f);
+	// Set camera position behind the position of an object and update it every frame
+	void Update () {
+		//because the script is inside the camera, the transform.position refers to the transfrom of the camera
+		transform.position = transformPlayer.position + offset; //camera transfrom equals the position of the object
+	}
+}
