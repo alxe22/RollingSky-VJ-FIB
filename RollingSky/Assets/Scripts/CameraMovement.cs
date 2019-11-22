e1e9bcdf34fa016669febebc7544b81a15c0cc38
roguib <roger.guasch.ibarra@est.fib.upc.edu>
2019-11-22 15:51:58 +0100

    Adapt camera position

--- RollingSky/Assets/Scripts/CameraMovement.cs
+++ RollingSky/Assets/Scripts/CameraMovement.cs
@@ -8,10 +8,8 @@ public class CameraMovement : MonoBehaviour {
 	*/
 
 	public Transform transformPlayer;
-	public Vector3 offset = new Vector3(0, 4.26f, -20.0f);
-	// Set camera position behind the position of an object and update it every frame
 	void Update () {
 		//because the script is inside the camera, the transform.position refers to the transfrom of the camera
-		transform.position = transformPlayer.position + offset; //camera transfrom equals the position of the object
+		transform.position = transform.position /*+ transformPlayer.position*/; //camera transfrom equals the position of the object
 	}
 }
--- RollingSky/Assets/Scripts/TestPlayerMovement.cs
+++ RollingSky/Assets/Scripts/TestPlayerMovement.cs
@@ -7,11 +7,11 @@ public class TestPlayerMovement : MonoBehaviour {
 This class handles player movement
 */
 	public Rigidbody rb;
-	public int forwardForce = 7000;
+	public int forwardForce = -7000;
 	public int middle = Screen.width/2;
 	
 	void FixedUpdate () {
 		rb.transform.rotation = Quaternion.Euler(0, 0, 0);
-		rb.AddForce(0, 0, 750*Time.deltaTime);
+		rb.AddForce(0, 0, -750*Time.deltaTime);
 	}
 }
