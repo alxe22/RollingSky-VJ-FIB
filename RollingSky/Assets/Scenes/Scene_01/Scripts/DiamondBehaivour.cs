c3ddc9d21e2c547dc9a09435c36c231feccb1e8a
roguib <roger.guasch.ibarra@est.fib.upc.edu>
2019-11-26 19:41:53 +0100

    Diamonds

---
+++ RollingSky/Assets/Scenes/Scene_01/Scripts/DiamondBehaivour.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiamondBehaivour : MonoBehaviour
+{
+    public bool enter = true;
+    public bool stay = true;
+    public bool exit = true;
+    public float moveSpeed;
+
+    void Awake()
+    {
+        // move and reduce the size of the cube
+        //transform.position = new Vector3(0, 0.25f, 0.75f);
+        //transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+
+        // add isTrigger
+        //var boxCollider = gameObject.AddComponent<BoxCollider>();
+        //boxCollider.isTrigger = true;
+
+        //moveSpeed = 1.0f;
+
+        // create a sphere for this cube to interact with
+        /*GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.gameObject.transform.position = new Vector3(0, 0, 0);
+        sphere.gameObject.AddComponent<Rigidbody>();
+        sphere.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+        sphere.gameObject.GetComponent<Rigidbody>().useGravity = false;*/
+    }
+
+    void Update()
+    {
+        //transform.Translate(Vector3.forward * Time.deltaTime * Input.GetAxis("Vertical") * moveSpeed);
+        //transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal") * moveSpeed);
+        //transform.Rotate = transform.Rotate + Quaternion.Euler(20);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (enter)
+        {
+            Debug.Log("entered");
+            //Destroy (transform.parent.gameObject, 0.1f); // destroy parent a few frames later;
+            Destroy(gameObject, 0.1f);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (exit)
+        {
+            Debug.Log("exit");
+        }
+    }
+}
---
+++ RollingSky/Assets/Scenes/Scene_01/Scripts/ObjectSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject diamond;
+    public TextAsset objectsPosition;
+
+    void createTile(char tile, Vector3 pos) {
+        Debug.Log("line readed : " + tile + " (" + pos.x + ' ' + pos.y + pos.z + ")");
+        if(tile == '1') {
+            GameObject objectInstance = Instantiate(diamond,pos,Quaternion.Euler(Vector3.left*90)) as GameObject;
+        }
+    }
+
+    void Start()
+    {
+     string[] lines = objectsPosition.text.Split('\n');
+        Debug.Log(lines.Length);
+        for (int i = 0; i < lines.Length; ++i) {
+            //createTile(lines[j][i],(float)i,(float)j);  //lines[j][i]
+            string[] coord = lines[i].Split(' ');
+            char tileType = char.Parse(coord[0]);
+            // [object, x, y, z]
+            createTile(char.Parse(coord[0]), new Vector3(float.Parse(coord[1]), float.Parse(coord[2]), float.Parse(coord[3])));
+        }
+    }
+}
