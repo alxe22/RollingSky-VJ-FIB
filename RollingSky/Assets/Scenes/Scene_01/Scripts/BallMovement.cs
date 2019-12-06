0326050fd10de97571143dc0b88ae2b2c6d4a2ed
roguib <roger.guasch.ibarra@est.fib.upc.edu>
2019-12-06 13:05:17 +0100

    Ball stops moving when there is a collision with an obstacle. Animated floating cube

--- RollingSky/Assets/Scenes/Scene_01/Scripts/BallMovement.cs
+++ RollingSky/Assets/Scenes/Scene_01/Scripts/BallMovement.cs
@@ -7,6 +7,7 @@ public class BallMovement : MonoBehaviour
     Vector3 offset = new Vector3(0.0f, 0.0f, -10.0f);
     public float degreesPerSecond = 120.0f, maxJumpHeight = 4.6f;
     private bool isJumping = false, isFalling = false;
+    private bool hasCollide = false;
 
     void Start()
     {
@@ -15,49 +16,51 @@ public class BallMovement : MonoBehaviour
 
 	void Update ()
     {
-        transform.position += (offset  * Time.deltaTime);
-        transform.Rotate(Vector3.left * 1000 * Time.deltaTime);
-        if (isJumping || isFalling) {
-            this.gameObject.GetComponent<Rigidbody>().useGravity = false;
-            if (isJumping && !isFalling && transform.position.y < maxJumpHeight) {
-                transform.position = new Vector3(transform.position.x, transform.position.y + 0.15f, transform.position.z);
+        if (!hasCollide) {
+            transform.position += (offset  * Time.deltaTime);
+            transform.Rotate(Vector3.left * 1000 * Time.deltaTime);
+            if (isJumping || isFalling) {
+                this.gameObject.GetComponent<Rigidbody>().useGravity = false;
+                if (isJumping && !isFalling && transform.position.y < maxJumpHeight) {
+                    transform.position = new Vector3(transform.position.x, transform.position.y + 0.15f, transform.position.z);
+                }
+                else if (isJumping && !isFalling && transform.position.y >= maxJumpHeight) {
+                    isJumping = false;
+                    isFalling = true;
+                }
+                else if (!isJumping && isFalling && transform.position.y > 0.375f) {
+                    transform.position = new Vector3(transform.position.x, transform.position.y - 0.15f, transform.position.z);
+                }
+                else if (!isJumping && isFalling && transform.position.y <= 0.375f) {
+                    isJumping = false;
+                    isFalling = false;
+                    transform.position = new Vector3(transform.position.x, 0.375f, transform.position.z);
+                }
             }
-            else if (isJumping && !isFalling && transform.position.y >= maxJumpHeight) {
-                isJumping = false;
-                isFalling = true;
-            }
-            else if (!isJumping && isFalling && transform.position.y > 0.375f) {
-                transform.position = new Vector3(transform.position.x, transform.position.y - 0.15f, transform.position.z);
-            }
-            else if (!isJumping && isFalling && transform.position.y <= 0.375f) {
-                isJumping = false;
-                isFalling = false;
-                transform.position = new Vector3(transform.position.x, 0.375f, transform.position.z);
-            }
-        }
-        else this.gameObject.GetComponent<Rigidbody>().useGravity = true;
-        //transform.Rotate(new Vector3(1, 0, 0) * degreesPerSecond * Time.deltaTime, Space.Self);
-        //if (transform.position.y > 0.373f) transform.rotation = Quaternion.Euler(0, 90, 0);
+            else this.gameObject.GetComponent<Rigidbody>().useGravity = true;
+            //transform.Rotate(new Vector3(1, 0, 0) * degreesPerSecond * Time.deltaTime, Space.Self);
+            //if (transform.position.y > 0.373f) transform.rotation = Quaternion.Euler(0, 90, 0);
 
-        if (Input.GetMouseButton(0)){
-          Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-          RaycastHit hit;
-          if (Physics.Raycast(ray, out hit)) {
-            Vector3 dir = hit.point - transform.position;
-            dir.z = 0;
-            dir.y = 0;
-            transform.Translate(dir * Time.deltaTime*5, Space.World);
+            if (Input.GetMouseButton(0)){
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit)) {
+                Vector3 dir = hit.point - transform.position;
+                dir.z = 0;
+                dir.y = 0;
+                transform.Translate(dir * Time.deltaTime*5, Space.World);
 
-            // transform.Translate (dir * Time.DeltaTime * speed); // Try this if it doesn't work
-          }
-        }
-        if(Input.GetKey("d")) {
-            transform.position += (new Vector3(-5f, 0f, 0f)  * Time.deltaTime);
-            //transform.Rotate(new Vector3(0, 1, 1) * 20f * Time.deltaTime, Space.Self);
-        }
-        else if(Input.GetKey("a")) {
-            transform.position += (new Vector3(5f, 0f, 0f)  * Time.deltaTime);
-            //transform.Rotate(new Vector3(0, 1, 1) * -20f * Time.deltaTime, Space.Self);
+                // transform.Translate (dir * Time.DeltaTime * speed); // Try this if it doesn't work
+            }
+            }
+            if(Input.GetKey("d")) {
+                transform.position += (new Vector3(-5f, 0f, 0f)  * Time.deltaTime);
+                //transform.Rotate(new Vector3(0, 1, 1) * 20f * Time.deltaTime, Space.Self);
+            }
+            else if(Input.GetKey("a")) {
+                transform.position += (new Vector3(5f, 0f, 0f)  * Time.deltaTime);
+                //transform.Rotate(new Vector3(0, 1, 1) * -20f * Time.deltaTime, Space.Self);
+            }
         }
     }
 
@@ -71,9 +74,33 @@ public class BallMovement : MonoBehaviour
             Debug.Log("Collision with: " + other.gameObject.name);
             if (!isJumping && !isFalling) isJumping = true;
         }
+        // floor walls
+        else if (other.gameObject.name == "lv01-wall-blue(Clone)" || 
+                other.gameObject.name == "lv01-wall-green(Clone)" || 
+                other.gameObject.name == "lv01-wall-red(Clone)" ||
+                other.gameObject.name == "lv01-wall-orange(Clone)" ||
+                other.gameObject.name == "lv01-wall-yellow(Clone)") {
+            Debug.Log("Wall");
+            obstacleCollision();
+        }
+        else if(other.gameObject.name == "floating-cube(Clone") {
+            Debug.Log("Cube");
+            obstacleCollision();
+        }
+        else if(other.gameObject.name == "lv01-obstacle-identity-disc(Clone)") {
+            Debug.Log("identity disc");
+            obstacleCollision();
+        }
+        else {
+            Debug.Log("There is any collision behaivour for: " + other.gameObject.name);
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
+    }
+
+    private void obstacleCollision() {
+         hasCollide = true;
     }
 }
