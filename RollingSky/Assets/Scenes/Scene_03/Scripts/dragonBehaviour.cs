1c9ebe28ef114595e05cfdeff268f7c9b6075784
roguib <roger.guasch.ibarra@est.fib.upc.edu>
2019-12-14 17:44:17 +0100

    Tiles and map

---
+++ RollingSky/Assets/Scenes/Scene_03/Scripts/dragonBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dragonBehaviour : MonoBehaviour
+{
+    private bool isMovingLeft = true;
+    private float speed = 0.01f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (transform.position.x <= -2f) isMovingLeft = true;
+        else isMovingLeft = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMovingLeft) {
+            transform.position = new Vector3(transform.position.x + speed, transform.position.y, transform.position.z);
+        }
+        else {
+            transform.position = new Vector3(transform.position.x - speed, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x <= -2f) isMovingLeft = true;
+        else if(transform.position.x >= 2f) isMovingLeft = false;
+    }
+}
---
+++ RollingSky/Assets/Scenes/Scene_03/Scripts/movingCobblestoneBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movingCobblestoneBehaviour : MonoBehaviour
+{
+    private bool moving = false;
+    private Vector3 initialPosition;
+    private Vector3 endPosition;
+    Vector3 offset = new Vector3(0.0f, 0.0f, -9.0f);
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (moving && endPosition.z <= transform.position.z) {
+            transform.position +=  offset * Time.deltaTime;
+        } 
+        else moving = false;
+    }
+
+    void Awake()
+    {
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        initialPosition = transform.position;
+        endPosition = transform.position;
+        endPosition.z = endPosition.z - 1;
+        moving = true;
+    }
+}
