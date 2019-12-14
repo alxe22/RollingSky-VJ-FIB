6eb6af0bd6c9fe94dda0677b3fd14d3bff9ea378
roguib <roger.guasch.ibarra@est.fib.upc.edu>
2019-12-14 18:57:57 +0100

    Dragon ball

---
+++ RollingSky/Assets/Scenes/Scene_03/Scripts/dragonBallBehaviour.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dragonBallBehaviour : MonoBehaviour
+{
+    
+    public bool enter = true;
+    public bool stay = true;
+    public bool exit = true;
+    public float moveSpeed;
+
+    public GameObject particleSystemContainer;
+    public GameObject defaultMat;
+
+    private bool collisionParticlesActivated = false;
+
+
+    ParticleSystem ps;
+
+    void Awake()
+    {
+        ps = particleSystemContainer.GetComponent<ParticleSystem>();
+        ps.Stop();
+    }
+
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (enter)
+        {
+            if (!collisionParticlesActivated) {
+                ps.Play();
+                defaultMat.GetComponent<MeshRenderer>().enabled = false;
+                collisionParticlesActivated = true;
+            }
+            Destroy(gameObject, 0.3f);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+    }
+}
