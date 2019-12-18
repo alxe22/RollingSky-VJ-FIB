087877d51972b9cc1da0d878e309db1982f7632b
alxe22 <40808122+alxe22@users.noreply.github.com>
2019-12-18 02:50:29 +0100

    FinalCommit

--- RollingSky/Assets/Scenes/Menu/Scripts/SceneManager.cs
+++ RollingSky/Assets/Scenes/Menu/Scripts/SceneManager.cs
@@ -12,6 +12,12 @@ public class SceneManager : MonoBehaviour
   public AudioClip SelectSound;
   public AudioSource Select;
 
+  public RawImage LoadScreen1;
+  public RawImage LoadScreen2;
+  public RawImage LoadScreen3;
+
+  private int scene;
+
   void onAwake() {
     Music.clip = BackGroundMusic;
     Select.clip = SelectSound;
@@ -21,7 +27,23 @@ public class SceneManager : MonoBehaviour
   public void ChangeScene(string name) {
     Music.Pause();
     Select.Play(0);
-    UnityEngine.SceneManagement.SceneManager.LoadScene(name, LoadSceneMode.Single);
+    if(name == "Scene_01") LoadScreen1.transform.localScale = new Vector3(1,1,1);
+    if(name == "Scene_02") LoadScreen2.transform.localScale = new Vector3(1,1,1);
+    if(name == "Scene_03") LoadScreen3.transform.localScale = new Vector3(1,1,1);
+
+    StartCoroutine(waiter(name));
   }
 
+  IEnumerator waiter(string name)
+  {
+    yield return new WaitForSeconds(5);
+    if(name == "Scene_01") UnityEngine.SceneManagement.SceneManager.LoadScene("Scene_01", LoadSceneMode.Single);
+    if(name == "Scene_02") UnityEngine.SceneManagement.SceneManager.LoadScene("Scene_02", LoadSceneMode.Single);
+    if(name == "Scene_03") UnityEngine.SceneManagement.SceneManager.LoadScene("Scene_03", LoadSceneMode.Single);
+  }
+
+
+  public void Quit() {
+     Application.Quit();
+  }
 }
